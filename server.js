b4077ddc64b230ac4b504ab28aa1ffd1c0c997ef
NAGPALADITI14 <138228231+NAGPALADITI14@users.noreply.github.com>
2024-10-29 16:35:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 //     .then(() => console.log('MongoDB connected'))
 //     .catch(err => console.error('MongoDB connection error:', err));
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const mongoURI = process.env.MONGODB_URI ;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
