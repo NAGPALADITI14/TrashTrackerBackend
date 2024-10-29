212369b5e399a891925d31ddb9de8cbad8890050
NAGPALADITI14 <138228231+NAGPALADITI14@users.noreply.github.com>
2024-10-29 16:31:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,12 +19,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/Cleanliness', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect('mongodb://localhost:27017/Cleanliness', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.error('MongoDB connection error:', err));
+
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGODB_URI ;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
 
 const storage = multer.diskStorage({
     destination: './uploads/',
